621788fc475dc97cf8813fc083df0bfa93fcd58c
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-11 11:31:50 +0100

    Init

---
+++ AdvancedCalculator/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace AdvancedCalculator {
+    partial class FRMAdvancedCalculatorDSte {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.TbxNumb1DSte = new System.Windows.Forms.TextBox();
+            this.TbxNumb2DSte = new System.Windows.Forms.TextBox();
+            this.LblOutputDSte = new System.Windows.Forms.Label();
+            this.BtnPlusDSte = new System.Windows.Forms.Button();
+            this.BtnTimesDSte = new System.Windows.Forms.Button();
+            this.BtnMinDSte = new System.Windows.Forms.Button();
+            this.BtnDevideDSte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TbxNumb1DSte
+            // 
+            this.TbxNumb1DSte.Location = new System.Drawing.Point(9, 8);
+            this.TbxNumb1DSte.Margin = new System.Windows.Forms.Padding(2);
+            this.TbxNumb1DSte.Name = "TbxNumb1DSte";
+            this.TbxNumb1DSte.Size = new System.Drawing.Size(154, 20);
+            this.TbxNumb1DSte.TabIndex = 0;
+            // 
+            // TbxNumb2DSte
+            // 
+            this.TbxNumb2DSte.Location = new System.Drawing.Point(216, 8);
+            this.TbxNumb2DSte.Margin = new System.Windows.Forms.Padding(2);
+            this.TbxNumb2DSte.Name = "TbxNumb2DSte";
+            this.TbxNumb2DSte.Size = new System.Drawing.Size(154, 20);
+            this.TbxNumb2DSte.TabIndex = 1;
+            // 
+            // LblOutputDSte
+            // 
+            this.LblOutputDSte.AutoSize = true;
+            this.LblOutputDSte.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblOutputDSte.Location = new System.Drawing.Point(172, 178);
+            this.LblOutputDSte.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblOutputDSte.Name = "LblOutputDSte";
+            this.LblOutputDSte.Size = new System.Drawing.Size(38, 31);
+            this.LblOutputDSte.TabIndex = 2;
+            this.LblOutputDSte.Text = "...";
+            this.LblOutputDSte.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnPlusDSte
+            // 
+            this.BtnPlusDSte.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlusDSte.Location = new System.Drawing.Point(98, 49);
+            this.BtnPlusDSte.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnPlusDSte.Name = "BtnPlusDSte";
+            this.BtnPlusDSte.Size = new System.Drawing.Size(65, 51);
+            this.BtnPlusDSte.TabIndex = 3;
+            this.BtnPlusDSte.Text = "+";
+            this.BtnPlusDSte.UseVisualStyleBackColor = true;
+            this.BtnPlusDSte.Click += new System.EventHandler(this.BtnPlusDSte_Click);
+            // 
+            // BtnTimesDSte
+            // 
+            this.BtnTimesDSte.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTimesDSte.Location = new System.Drawing.Point(216, 49);
+            this.BtnTimesDSte.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnTimesDSte.Name = "BtnTimesDSte";
+            this.BtnTimesDSte.Size = new System.Drawing.Size(65, 51);
+            this.BtnTimesDSte.TabIndex = 4;
+            this.BtnTimesDSte.Text = "X";
+            this.BtnTimesDSte.UseVisualStyleBackColor = true;
+            this.BtnTimesDSte.Click += new System.EventHandler(this.BtnTimesDSte_Click);
+            // 
+            // BtnMinDSte
+            // 
+            this.BtnMinDSte.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMinDSte.Location = new System.Drawing.Point(98, 104);
+            this.BtnMinDSte.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnMinDSte.Name = "BtnMinDSte";
+            this.BtnMinDSte.Size = new System.Drawing.Size(65, 51);
+            this.BtnMinDSte.TabIndex = 5;
+            this.BtnMinDSte.Text = "-";
+            this.BtnMinDSte.UseVisualStyleBackColor = true;
+            this.BtnMinDSte.Click += new System.EventHandler(this.BtnMinDSte_Click);
+            // 
+            // BtnDevideDSte
+            // 
+            this.BtnDevideDSte.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDevideDSte.Location = new System.Drawing.Point(216, 104);
+            this.BtnDevideDSte.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnDevideDSte.Name = "BtnDevideDSte";
+            this.BtnDevideDSte.Size = new System.Drawing.Size(65, 51);
+            this.BtnDevideDSte.TabIndex = 6;
+            this.BtnDevideDSte.Text = "/";
+            this.BtnDevideDSte.UseVisualStyleBackColor = true;
+            this.BtnDevideDSte.Click += new System.EventHandler(this.BtnDevideDSte_Click);
+            // 
+            // FRMAdvancedCalculatorDSte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(381, 248);
+            this.Controls.Add(this.BtnDevideDSte);
+            this.Controls.Add(this.BtnMinDSte);
+            this.Controls.Add(this.BtnTimesDSte);
+            this.Controls.Add(this.BtnPlusDSte);
+            this.Controls.Add(this.LblOutputDSte);
+            this.Controls.Add(this.TbxNumb2DSte);
+            this.Controls.Add(this.TbxNumb1DSte);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "FRMAdvancedCalculatorDSte";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.FRMAdvancedCalculatorDSte_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbxNumb1DSte;
+        private System.Windows.Forms.TextBox TbxNumb2DSte;
+        private System.Windows.Forms.Label LblOutputDSte;
+        private System.Windows.Forms.Button BtnPlusDSte;
+        private System.Windows.Forms.Button BtnTimesDSte;
+        private System.Windows.Forms.Button BtnMinDSte;
+        private System.Windows.Forms.Button BtnDevideDSte;
+    }
+}
+
---
+++ AdvancedCalculator/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedCalculator {
+    public partial class FRMAdvancedCalculatorDSte : Form {
+        public FRMAdvancedCalculatorDSte() {
+            InitializeComponent();
+        }
+
+        private void FRMAdvancedCalculatorDSte_Load(object sender, EventArgs e) {
+            LblOutputDSte.Text = "";
+        }
+
+        private void BtnPlusDSte_Click(object sender, EventArgs e) {
+            CalculateDSte(TbxNumb1DSte.Text, "+", TbxNumb2DSte.Text);
+        }
+
+        private void BtnTimesDSte_Click(object sender, EventArgs e) {
+            CalculateDSte(TbxNumb1DSte.Text, "*", TbxNumb2DSte.Text);
+
+        }
+
+        private void BtnMinDSte_Click(object sender, EventArgs e) {
+            CalculateDSte(TbxNumb1DSte.Text, "-", TbxNumb2DSte.Text);
+        }
+
+        private void BtnDevideDSte_Click(object sender, EventArgs e) {
+            CalculateDSte(TbxNumb1DSte.Text, "/", TbxNumb2DSte.Text);
+        }
+
+        private void CalculateDSte(String number1, String Operator, String number2) {
+
+            if (number1 == "" || number2 == "") {
+                MessageBox.Show("Please enter two numbers.");
+                return;
+            }
+
+            double num1 = ToIntDSte(number1);
+            double num2 = ToIntDSte(number2);
+
+            switch (Operator) {
+                case "+":
+                    LblOutputDSte.Text = "" + (num1 + num2);
+                    break;
+                case "*":
+                    LblOutputDSte.Text = "" + (num1 * num2);
+                    break;
+                case "-":
+                    LblOutputDSte.Text = "" + (num1 - num2);
+                    break;
+                case "/":
+                    if (num1 != 0 && num2 != 0) {
+                        LblOutputDSte.Text = "" + (num1 / num2);
+                    } else {
+                        MessageBox.Show("WHOOPS, can't divide by 0 dumy");
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Unknown value entered");
+                    break;
+            }
+        }
+
+        private int ToIntDSte(String num) {
+            try {
+                return Convert.ToInt32(num);
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.StackTrace);
+                return 1;
+            }
+        }
+    }
+}
